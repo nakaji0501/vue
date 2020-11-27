939a810d40c24e73ecedf61a9058d8dfe584bf79
nakaji <nakajinia0501@gmail.com>
2020-11-28 07:04:07 +0900

    routes/web.php + webpack.mix.jsを変更しました

--- noticeboard/web/laravel/webpack.mix.js
+++ noticeboard/web/laravel/webpack.mix.js
@@ -11,7 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+
+const mix = require('laravel-mix')
+
+mix.browserSync({
+    proxy: '0.0.0.0:8081', // アプリの起動アドレス
+    open: false // ブラウザを自動で開かない
+  })
+  .js('resources/js/app.js', 'public/js')
+  .version()
