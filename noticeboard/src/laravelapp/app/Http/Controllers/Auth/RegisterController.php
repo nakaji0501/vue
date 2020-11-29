bf4932471eedaf416a2305d8f2ac549970da27d2
nakaji <nakajinia0501@gmail.com>
2020-11-30 06:03:09 +0900

    RegisterApiのルート作成しました。Testコードで正常にレスポンスされました。

--- noticeboard/src/laravelapp/app/Http/Controllers/Auth/RegisterController.php
+++ noticeboard/src/laravelapp/app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -70,4 +71,10 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    protected function registered(Request $request, $user)
+    {
+        return $user;
+    }
+
 }
--- noticeboard/src/laravelapp/routes/api.php
+++ noticeboard/src/laravelapp/routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
