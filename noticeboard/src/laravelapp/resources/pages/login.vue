af1530956c6b207eedb81feda10cbd0ef63c0afa
nakaji <nakajinia0501@gmail.com>
2020-12-01 05:08:04 +0900

    loginPageを編集しました

--- noticeboard/src/laravelapp/resources/pages/login.vue
+++ noticeboard/src/laravelapp/resources/pages/login.vue
@@ -1,3 +1,53 @@
 <template>
-    <h1>Login</h1>
+    <div class="container--small">
+
+        <ul class="tab">
+            <li class="tab__item"
+            :class="{'tab__item-active': tab === 1}"
+            @click="tab = 1"
+            >
+            Login
+            </li>
+            <li class="tab__item"
+            :class="{'tab__item-active': tab === 2}"
+            @click="tab = 2"
+            >Register
+            </li>
+        </ul>
+
+        <div class="panel" v-show="tab === 1">
+        <form class="form" @submit.prevent="login">
+            <label for="login-email">Email</label>
+            <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+            <label for="login-password">Password</label>
+            <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+            <div class="form__button">
+            <button type="submit" class="button button--inverse">login</button>
+            </div>
+        </form>
+        </div>
+
+        <div class="panel" v-show="tab === 2"
+        >Register Form</div>
+
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            tab: 1,
+            loginForm: {
+                email: '',
+                password: '',
+            },
+        }
+    },
+    methods: {
+        login() {
+            console.log(this.loginForm);
+        }
+    }
+}
+</script>
