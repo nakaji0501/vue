08ed493247bc9c021195be4980b179bc85981e11
nakaji <nakajinia0501@gmail.com>
2020-12-01 04:08:07 +0900

    headerのNavbarテンプレートを 作りました

--- noticeboard/src/laravelapp/resources/js/App.vue
+++ noticeboard/src/laravelapp/resources/js/App.vue
@@ -1,9 +1,25 @@
 <template>
     <div>
+
+        <header>
+            <Navbar />
+        </header>
+
         <main>
             <div class="container">
                 <router-view />
             </div>
         </main>
+
     </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+    components: {
+        Navbar,
+    }
+}
+</script>
