c2e36f28ce0629c0f64ebb5245c706fdb7143277
nakaji <nakajinia0501@gmail.com>
2020-11-30 22:30:04 +0900

    API Login Logout 作成とテストコード成功しました

--- noticeboard/src/laravelapp/routes/api.php
+++ noticeboard/src/laravelapp/routes/api.php
@@ -20,3 +20,9 @@ use Illuminate\Http\Request;
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
 
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
