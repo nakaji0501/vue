bb670b7ae7bafbb8b5f2d78f7db92f5d704443c5
nakaji <nakajinia0501@gmail.com>
2020-12-01 05:24:58 +0900

    registerFormを編集しました

--- noticeboard/src/laravelapp/public/js/app.js
+++ noticeboard/src/laravelapp/public/js/app.js
@@ -170,6 +170,19 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -177,12 +190,21 @@ __webpack_require__.r(__webpack_exports__);
       loginForm: {
         email: '',
         password: ''
+      },
+      registerForm: {
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
       }
     };
   },
   methods: {
     login: function login() {
       console.log(this.loginForm);
+    },
+    register: function register() {
+      console.log(this.registerForm);
     }
   }
 });
@@ -961,7 +983,125 @@ var render = function() {
         ],
         staticClass: "panel"
       },
-      [_vm._v("Register Form")]
+      [
+        _c(
+          "form",
+          {
+            staticClass: "form",
+            on: {
+              submit: function($event) {
+                $event.preventDefault()
+                return _vm.register($event)
+              }
+            }
+          },
+          [
+            _c("label", { attrs: { for: "username" } }, [_vm._v("Name")]),
+            _vm._v(" "),
+            _c("input", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.registerForm.name,
+                  expression: "registerForm.name"
+                }
+              ],
+              staticClass: "form__item",
+              attrs: { type: "text", id: "username" },
+              domProps: { value: _vm.registerForm.name },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.$set(_vm.registerForm, "name", $event.target.value)
+                }
+              }
+            }),
+            _vm._v(" "),
+            _c("label", { attrs: { for: "email" } }, [_vm._v("Email")]),
+            _vm._v(" "),
+            _c("input", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.registerForm.email,
+                  expression: "registerForm.email"
+                }
+              ],
+              staticClass: "form__item",
+              attrs: { type: "text", id: "email" },
+              domProps: { value: _vm.registerForm.email },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.$set(_vm.registerForm, "email", $event.target.value)
+                }
+              }
+            }),
+            _vm._v(" "),
+            _c("label", { attrs: { for: "password" } }, [_vm._v("Password")]),
+            _vm._v(" "),
+            _c("input", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.registerForm.password,
+                  expression: "registerForm.password"
+                }
+              ],
+              staticClass: "form__item",
+              attrs: { type: "password", id: "password" },
+              domProps: { value: _vm.registerForm.password },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.$set(_vm.registerForm, "password", $event.target.value)
+                }
+              }
+            }),
+            _vm._v(" "),
+            _c("label", { attrs: { for: "password-confirmation" } }, [
+              _vm._v("Password (confirm)")
+            ]),
+            _vm._v(" "),
+            _c("input", {
+              directives: [
+                {
+                  name: "model",
+                  rawName: "v-model",
+                  value: _vm.registerForm.password_confirmation,
+                  expression: "registerForm.password_confirmation"
+                }
+              ],
+              staticClass: "form__item",
+              attrs: { type: "password", id: "password-confirmation" },
+              domProps: { value: _vm.registerForm.password_confirmation },
+              on: {
+                input: function($event) {
+                  if ($event.target.composing) {
+                    return
+                  }
+                  _vm.$set(
+                    _vm.registerForm,
+                    "password_confirmation",
+                    $event.target.value
+                  )
+                }
+              }
+            }),
+            _vm._v(" "),
+            _vm._m(1)
+          ]
+        )
+      ]
     )
   ])
 }
@@ -975,6 +1115,18 @@ var staticRenderFns = [
         "button",
         { staticClass: "button button--inverse", attrs: { type: "submit" } },
         [_vm._v("login")]
+      )
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "form__button" }, [
+      _c(
+        "button",
+        { staticClass: "button button--inverse", attrs: { type: "submit" } },
+        [_vm._v("register")]
       )
     ])
   }
--- noticeboard/src/laravelapp/resources/pages/login.vue
+++ noticeboard/src/laravelapp/resources/pages/login.vue
@@ -16,19 +16,32 @@
         </ul>
 
         <div class="panel" v-show="tab === 1">
-        <form class="form" @submit.prevent="login">
-            <label for="login-email">Email</label>
-            <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
-            <label for="login-password">Password</label>
-            <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
-            <div class="form__button">
-            <button type="submit" class="button button--inverse">login</button>
-            </div>
-        </form>
+            <form class="form" @submit.prevent="login">
+                <label for="login-email">Email</label>
+                <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+                <label for="login-password">Password</label>
+                <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+                <div class="form__button">
+                <button type="submit" class="button button--inverse">login</button>
+                </div>
+            </form>
         </div>
 
-        <div class="panel" v-show="tab === 2"
-        >Register Form</div>
+        <div class="panel" v-show="tab === 2">
+            <form class="form" @submit.prevent="register">
+                <label for="username">Name</label>
+                <input type="text" class="form__item" id="username" v-model="registerForm.name">
+                <label for="email">Email</label>
+                <input type="text" class="form__item" id="email" v-model="registerForm.email">
+                <label for="password">Password</label>
+                <input type="password" class="form__item" id="password" v-model="registerForm.password">
+                <label for="password-confirmation">Password (confirm)</label>
+                <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+                <div class="form__button">
+                <button type="submit" class="button button--inverse">register</button>
+                </div>
+            </form>
+        </div>
 
     </div>
 </template>
@@ -42,12 +55,21 @@ export default {
                 email: '',
                 password: '',
             },
+            registerForm: {
+                name: '',
+                email: '',
+                password: '',
+                password_confirmation: ''
+            }
         }
     },
     methods: {
         login() {
             console.log(this.loginForm);
-        }
+        },
+        register() {
+            console.log(this.registerForm);
+        },
     }
 }
 </script>
