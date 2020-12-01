fb0d0c4af88e2efac626ee5486b7449a1f51d3c7
nakaji <nakajinia0501@gmail.com>
2020-12-02 03:29:06 +0900

    ナビゲーションガードを追加しました

--- noticeboard/src/laravelapp/resources/js/router.js
+++ noticeboard/src/laravelapp/resources/js/router.js
@@ -4,6 +4,8 @@ import VueRouter from 'vue-router'
 import PhotoList from './pages/PhotoList.vue'
 import Login from './pages/Login.vue'
 
+import store from './store'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,8 +15,15 @@ const routes = [
     },
     {
         path: '/login',
-        component: Login
-    }
+        component: Login,
+        beforeEnter (to, from, next) {
+            if (store.getters['auth/check']) {
+                next('/')
+            } else {
+                next()
+            }
+        }
+    },
 ]
 
 const router = new VueRouter({
