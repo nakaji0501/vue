6919891e2b34434279b4ea8e94c6cebca88832c9
nakaji <nakajinia0501@gmail.com>
2020-12-02 03:34:12 +0900

    システムエラーの設定を追加しました

--- noticeboard/src/laravelapp/resources/js/router.js
+++ noticeboard/src/laravelapp/resources/js/router.js
@@ -5,6 +5,7 @@ import PhotoList from './pages/PhotoList.vue'
 import Login from './pages/Login.vue'
 
 import store from './store'
+import SystemError from './pages/errors/System.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,10 @@ const routes = [
                 next()
             }
         }
+    },
+    {
+        path: '/500',
+        component: SystemError
     },
 ]
 
--- noticeboard/src/laravelapp/resources/js/util.js
+++ noticeboard/src/laravelapp/resources/js/util.js
@@ -19,3 +19,7 @@ export function getCookieValue (searchKey) {
 
   return val
 }
+
+export const OK = 200
+export const CREATED = 201
+export const INTERNAL_SERVER_ERROR = 500
