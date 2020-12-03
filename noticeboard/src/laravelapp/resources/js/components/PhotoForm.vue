cb282480dce417071618b60f98a3c091faa49340
nakaji <nakajinia0501@gmail.com>
2020-12-04 04:44:09 +0900

    submit Photo Form表示部分と@clickで表示ONOFFを作りました

---
+++ noticeboard/src/laravelapp/resources/js/components/PhotoForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="photo-form"
+    v-show="value">
+
+        <h2 class="title">
+            Submit a Photo
+        </h2>
+
+        <form class="form">
+            <input type="file" class="form__item">
+
+            <div class="form__button">
+                <button class="button button--inverse">
+                    submit
+                </button>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: Boolean,
+            required: true
+        }
+    }
+}
+</script>
