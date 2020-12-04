b9c92c8218ff6ce8e185902eb774b04ab2652871
nakaji <nakajinia0501@gmail.com>
2020-12-05 06:22:13 +0900

    写真一覧をテンプレートを作成しました

---
+++ noticeboard/src/laravelapp/resources/js/components/Photo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="photo">
+        <figure class="photo__wrapper">
+            <img class="photo__image" src="item.url" alt="`Photo by ${item.owner.name}`">
+        </figure>
+        <router-link class="photo__overlay"
+        :to="`/photos/${item.id}`"
+        :title="`View the photo by ${item.owner.name}`"
+        >
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
