0ebd2546805dda0fa3a889ba19011ed5e7816839
nakaji <nakajinia0501@gmail.com>
2020-12-05 06:28:46 +0900

    い

--- noticeboard/src/laravelapp/resources/js/components/Photo.vue
+++ noticeboard/src/laravelapp/resources/js/components/Photo.vue
@@ -1,12 +1,21 @@
 <template>
     <div class="photo">
+
         <figure class="photo__wrapper">
             <img class="photo__image" src="item.url" alt="`Photo by ${item.owner.name}`">
         </figure>
+
         <router-link class="photo__overlay"
         :to="`/photos/${item.id}`"
         :title="`View the photo by ${item.owner.name}`"
         >
+
+        <div class="photo__controls">
+            <button class="photo__action photo__action--like"
+            title="Like Photo">
+                <i class="icon ion-md-heart"></i>12
+            </button>
+        </div>
         </router-link>
     </div>
 </template>
