2b6aad60573f980bc504d81bc033c09aa0926e90
nakaji <nakajinia0501@gmail.com>
2020-12-05 06:05:34 +0900

    ダウンロード機能を追加しました

--- noticeboard/src/laravelapp/app/Http/Controllers/PhotoController.php
+++ noticeboard/src/laravelapp/app/Http/Controllers/PhotoController.php
@@ -14,7 +14,7 @@ class PhotoController extends Controller
     public function __construct()
     {
         // 認証が必要
-        $this->middleware('auth')->except(['index']);
+        $this->middleware('auth')->except(['index', 'download']);
     }
 
     /**
@@ -68,4 +68,24 @@ class PhotoController extends Controller
         return $photos;
     }
 
+    /**
+     * 写真ダウンロード
+     * @param Photo $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function download(Photo $photo)
+    {
+        // 写真の存在チェック
+        if (! Storage::cloud()->exists($photo->filename)) {
+            abort(404);
+        }
+
+        $disposition = 'attachment; filename="' . $photo->filename . '"';
+        $headers = [
+            'Content-Type' => 'application/octet-stream',
+            'Content-Disposition' => $disposition,
+        ];
+
+        return response(Storage::cloud()->get($photo->filename), 200, $headers);
+    }
 }
