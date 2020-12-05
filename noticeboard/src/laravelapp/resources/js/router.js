fe308a2367b5486a59eb38383b70039e29b43b1e
nakaji <nakajinia0501@gmail.com>
2020-12-05 15:21:55 +0900

    ページネーションのルート定義を記述しました

--- noticeboard/src/laravelapp/resources/js/router.js
+++ noticeboard/src/laravelapp/resources/js/router.js
@@ -13,7 +13,11 @@ Vue.use(VueRouter)
 const routes = [
     {
         path: '/',
-        component: PhotoList
+        component: PhotoList,
+        props: route => {
+            const page = route.query.page
+            return { page: /^[1-9][0-9]*$/.test(page) ? page * 1 : 1}
+        },
     },
     {
         path: '/photos/:id',
