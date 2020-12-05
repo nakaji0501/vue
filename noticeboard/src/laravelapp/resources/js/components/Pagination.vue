4071115277dc6f9b38b55a8227fb510917605e00
nakaji <nakajinia0501@gmail.com>
2020-12-05 15:28:32 +0900

    Paginationコンポーネントを作成しました

---
+++ noticeboard/src/laravelapp/resources/js/components/Pagination.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="pagination">
+
+        <router-link
+        v-if="! isFirstPage"
+        :to="`/?page=${currentPage - 1}`"
+        class="button"
+        >
+        &laquo; prev
+        </router-link>
+
+        <router-link
+        v-if="! isLastPage"
+        :to="`/?page=${currentPage + 1}`"
+        class="button"
+        >
+        next &raquo;
+        </router-link>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentPage: {
+            type: Number,
+            required: true,
+        },
+        lastPage: {
+            type: Number,
+            required: true
+        },
+    },
+    computed: {
+        isFirstPage() {
+            return this.currentPage === 1
+        },
+        isLastPage() {
+            return this.currentPage === this.lastPage
+        },
+    },
+}
+</script>
