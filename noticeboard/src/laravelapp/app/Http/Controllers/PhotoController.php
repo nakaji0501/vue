b5422af1bd1a63a867e77d84c266375c49a79c10
nakaji <nakajinia0501@gmail.com>
2020-12-05 15:54:29 +0900

    写真詳細のWEBAPIの実装とテストコードを作成

--- noticeboard/src/laravelapp/app/Http/Controllers/PhotoController.php
+++ noticeboard/src/laravelapp/app/Http/Controllers/PhotoController.php
@@ -14,7 +14,7 @@ class PhotoController extends Controller
     public function __construct()
     {
         // 認証が必要
-        $this->middleware('auth')->except(['index', 'download']);
+        $this->middleware('auth')->except(['index', 'download', 'show']);
     }
 
     /**
@@ -88,4 +88,16 @@ class PhotoController extends Controller
 
         return response(Storage::cloud()->get($photo->filename), 200, $headers);
     }
+
+    /**
+ * 写真詳細
+ * @param string $id
+ * @return Photo
+ */
+public function show(string $id)
+{
+    $photo = Photo::where('id', $id)->with(['owner'])->first();
+
+    return $photo ?? abort(404);
+}
 }
